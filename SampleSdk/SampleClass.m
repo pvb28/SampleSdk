988b04bb9f7eca49c1910ea51ab9fb7faa49d890
Veera Brahmam Pydimarri <veera.pydimarri@zohocorp.com>
2021-11-25 17:10:48 +0530

    uploaded files

---
+++ SampleSdk/SampleClass.m
@@ -0,0 +1,17 @@
+//
+//  SampleClass.m
+//  SampleSdk
+//
+//  Created by Veera Brahmam Pydimarri on 24/11/21.
+//
+
+#import "SampleClass.h"
+
+@implementation SampleClass
+
++ (void) customPrint:(NSString *)customString{
+    NSLog(@"%@",customString);
+}
+
+
+@end
