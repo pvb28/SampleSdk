eae080ed5e379192ae2dd8d49f3fd7ac0c69f244
Veera Brahmam Pydimarri <veera.pydimarri@zohocorp.com>
2021-11-26 13:49:42 +0530

    adding podspec

---
+++ SampleSdk_MacOS/SampleSdk_MacOS.h
@@ -0,0 +1,17 @@
+//
+//  SampleSdk_MacOS.h
+//  SampleSdk_MacOS
+//
+//  Created by Veera Brahmam Pydimarri on 26/11/21.
+//
+
+
+//! Project version number for SampleSdk_MacOS.
+FOUNDATION_EXPORT double SampleSdk_MacOSVersionNumber;
+
+//! Project version string for SampleSdk_MacOS.
+FOUNDATION_EXPORT const unsigned char SampleSdk_MacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleSdk_MacOS/PublicHeader.h>
+
+
